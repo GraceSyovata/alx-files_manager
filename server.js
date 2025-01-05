33142ec32f260e51b9b515137d03196c78378502
CPA Grace Syovata <123548408+GraceSyovata@users.noreply.github.com>
2025-01-05 13:44:19 +0300

    manager

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import routes from './routes';
+import controllerRouting from './routes/index';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(routes);
+controllerRouting(app);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+export default app;
